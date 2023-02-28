a65bb8de1f07fd9f82363e722a65bbc2a9a9680a
versusdec <versusdec@gmail.com>
2023-02-28 10:57:13 +0200

    Back up

--- src/layouts/auth.js
+++ src/layouts/auth.js
@@ -26,32 +26,6 @@ export const Layout = (props) => {
       >
         <Box
           sx={{
-            backgroundColor: 'background.paper',
-            display: 'flex',
-            flex: {
-              xs: '1 1 auto',
-              md: '0 0 auto'
-            },
-            flexDirection: 'column',
-            justifyContent: {
-              md: 'center'
-            },
-            maxWidth: '100%',
-            p: {
-              xs: 4,
-              md: 8
-            },
-            width: {
-              md: 500
-            }
-          }}
-        >
-          <div>
-            {children}
-          </div>
-        </Box>
-        <Box
-          sx={{
             alignItems: 'center',
             backgroundColor: '#222223',
             backgroundImage: 'url("/assets/auth_bg.svg")',
@@ -73,7 +47,32 @@ export const Layout = (props) => {
         >
           <img src="/assets/logo.svg" alt="" style={{maxWidth: '600px'}} />
         </Box>
-  
+        <Box
+          sx={{
+            backgroundColor: 'background.paper',
+            display: 'flex',
+            flex: {
+              xs: '1 1 auto',
+              md: '0 0 auto'
+            },
+            flexDirection: 'column',
+            justifyContent: {
+              md: 'center'
+            },
+            maxWidth: '100%',
+            p: {
+              xs: 4,
+              md: 8
+            },
+            width: { 
+              md: 500
+            }
+          }}
+        >
+          <div>
+            {children}
+          </div>
+        </Box>
       </Box>
     </>
   );
